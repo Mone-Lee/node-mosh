62c5acb36f15bba03dbe4680f5bd352d5ec15598
Mone-Lee <1051933474@qq.com>
2025-01-24 12:30:57 +0800

    feat: http instance

--- first-app/http-demo.js
+++ first-app/http-demo.js
@@ -2,10 +2,18 @@ const http = require('http');
 
 const PORT = 3000;
 
+// server 是一个eventEmitter实例
+// 注意req和res实际是stream实例，这使得可以实现req.pipe(res)和req.on('data', (chunk) => {})这样的操作
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ message: 'Hello World' }));
 })
+
+// 上面等同于
+// server.on('request', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(JSON.stringify({ message: 'Hello World' }));
+// })
 
 server.listen(3000, () => {
   console.log(`Server is listening on port ${PORT}`);
